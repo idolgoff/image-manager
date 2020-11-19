0a2a58ed3be918d4e6e56bc1c34ee74328eb10fb
Ivan Dolgov <i.dolgov@orbita.center>
2020-11-19 14:33:06 +0300

    Ready for test in image mode

--- src/routes/__tests__/api.test.js
+++ src/routes/__tests__/api.test.js
@@ -9,15 +9,16 @@ describe('test service api', () => {
     });
 
     describe('request GET /', () => {
-        it('should return 404', async () => {
-            expect.assertions(1);
+        it('should return {ok: true}', async () => {
+            expect.assertions(2);
 
             const response = await app.inject({
                 method: 'GET',
                 url: '/',
             });
 
-            expect(response.statusCode).toBe(404);
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toStrictEqual('{"ok":true}');
         });
     });
 
@@ -42,6 +43,30 @@ describe('test service api', () => {
             });
             expect(response.statusCode).toBe(200);
             expect(response.json()).toHaveProperty('jobId');
+        });
+    });
+
+    describe('request GET /v1/queue/image', () => {
+        it('should register job and return its status', async () => {
+            expect.assertions(2);
+            const response = await app.inject({
+                method: 'POST',
+                url: '/v1/queue/image',
+                payload: {
+                    imageUrl: 'https://miro.medium.com/max/2880/1*Ar1k9HjU8Rhq1tqA6GEcdg.jpeg',
+                    webHook: '/test-webhook',
+                },
+            });
+            expect(response.statusCode).toBe(200);
+
+            const jobId = response.json()['jobId'];
+            const responseGet = await app.inject({
+                method: 'GET',
+                url: '/v1/queue/image',
+                query: {jobId},
+            });
+
+            console.log(responseGet);
         });
     });
 
